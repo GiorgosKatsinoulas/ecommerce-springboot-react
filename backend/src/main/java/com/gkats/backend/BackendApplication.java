be684245e9ab9e246e80d5159caa5cd28fa99fb2
gkatsinoulas <gkatsinoulas@cognity.gr>
2024-11-21 14:17:50 +0200

    Removing unnecessary exclude for DB

--- backend/src/main/java/com/gkats/backend/BackendApplication.java
+++ backend/src/main/java/com/gkats/backend/BackendApplication.java
@@ -2,10 +2,10 @@ package com.gkats.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//@SpringBootApplication
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) // for testing at first without db connection
 public class BackendApplication {
 
 	public static void main(String[] args) {
