a3282c606c07838f046194869afa96ff75557e7a
gkatsinoulas <gkatsinoulas@cognity.gr>
2024-11-21 14:18:23 +0200

    Create the Product object

---
+++ backend/src/main/java/com/gkats/backend/model/Product.java
@@ -0,0 +1,28 @@
+package com.gkats.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String category;
+    private String imageurl;
+    private Boolean available;
+    private String SKU;
+    private String dimensions;
+    private String color;
+
+}
