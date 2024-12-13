bd415241e6a3435a136d3ffe0e08b3273c06875a
gkatsinoulas <gkatsinoulas@cognity.gr>
2024-12-13 14:01:45 +0200

    Adding Builder on ApiResponse

--- backend/src/main/java/com/gkats/backend/utils/ApiResponse.java
+++ backend/src/main/java/com/gkats/backend/utils/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.gkats.backend.utils;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ApiResponse<T> {
     private int status;
     private String message;
