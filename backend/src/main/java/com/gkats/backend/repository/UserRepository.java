0a479fa5df006fc381d69213d713013937cc01ef
gkatsinoulas <gkatsinoulas@cognity.gr>
2024-12-13 13:22:53 +0200

    Add User repository

---
+++ backend/src/main/java/com/gkats/backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.gkats.backend.repository;
+
+
+import com.gkats.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
